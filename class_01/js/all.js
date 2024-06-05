42e4ee81f55e7762cfc74fa9995de29d9ff5f13f
Shelley <jialing80916@gmail.com>
2024-06-05 18:18:03 +0800

    learned how to used return

--- class_01/js/all.js
+++ class_01/js/all.js
@@ -1,18 +1,15 @@
 
-document.getElementById('countID').onclick = function(){
-    var hamPrice = 55;
-    var drinkPrice = 35;
-    var hamburgerNum = parseInt(document.getElementById('hamburgerID').value) * hamPrice;
-    var drinkNum = parseInt(document.getElementById('drinkID').value) * drinkPrice;
-    document.getElementById('totalID').textContent = hamburgerNum + drinkNum;
-    console.log(typeof(hamburgerNum + drinkNum));
-}
-
-/*
 function getTotalPrice(xNum, yNum){
     var hamPrice = 55;
     var drinkPrice = 35;
     var totalPrice = (hamPrice * xNum) + (drinkPrice * yNum);
-    return total;
+    return totalPrice;
 }
-*/
+
+document.getElementById('countID').onclick = function(){
+    var hamburgerNum = parseInt(document.getElementById('hamburgerID').value);
+    var drinkNum = parseInt(document.getElementById('drinkID').value);
+
+    var clientOrder = getTotalPrice(hamburgerNum, drinkNum);
+    document.getElementById('totalID').textContent = clientOrder;
+}
