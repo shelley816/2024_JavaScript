71121918f52d48dc17c2d5ff8430e13facf8364f
Shelley <jialing80916@gmail.com>
2024-06-05 21:18:12 +0800

    Updates

--- js/all.js
+++ js/all.js
@@ -1,2 +1,15 @@
-// alert('Hello World')
-document.getElementById('title1').textContent = '修改後標題1';
+
+var oliveGroved = [];
+
+oliveGroved.push(3);
+oliveGroved.push(16);
+oliveGroved.push(4);
+oliveGroved.push(7);
+oliveGroved.push(9);
+oliveGroved.push(2);
+oliveGroved.push(30);
+
+oliveGroved[0] = 5;
+
+console.log('我總共有' + oliveGroved.length + '顆橄欖樹');
+console.log(oliveGroved);
