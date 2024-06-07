e04ee1acc4460089defd10282df47aebfc59832f
Shelley <jialing80916@gmail.com>
2024-06-07 20:09:42 +0800

    how to use array+object

--- class_01/js/all.js
+++ class_01/js/all.js
@@ -1,18 +1,16 @@
 
-var store = {
-    manager: 'John',
-    productsSort: ['classic', 'casual', 'special'],
-    classicItem: 20,
-    casualItem: 35,
-    specialItem: 10,
-    recommend: function(){
-        console.log(store.productsSort[0] + '是經典系列，' + store.productsSort[1] + '則是比較休閒的款式。')
-    },
-    totalItem: function(){
-        var num = store.classicItem + store.casualItem + store.specialItem;
-        console.log('總共有' + num + '個商品。')
+var store = [
+    {
+        manager: 'John',
+        year: 8,
+        staffName: ['Mary', 'Bibi', 'Fifi'],
+        productItem: 76
+    },{
+        manager: 'Lily',
+        year: 5,
+        staffName: ['Bobo', 'Karen', 'Shelly'],
+        productItem: 89
     }
-}
+];
 
-store.recommend();
-store.totalItem();
+console.log(store[1].staffName[2]);
