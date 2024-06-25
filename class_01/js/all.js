bc138adcb806be3b44ccf5a3b619776e2db5724b
Shelley <jialing80916@gmail.com>
2024-06-25 19:01:17 +0800

    learn to change HTML attribute

--- class_01/js/all.js
+++ class_01/js/all.js
@@ -1,7 +1,4 @@
-// var el = document.getElementById('titleID'); // 讀取獨一無二的 ID，每個標籤只能有一個 ID
-// var el = document.querySelector('.titleClass span'); // 可抓標籤名稱, ID, Class... 只取第一筆資料
-var el = document.querySelectorAll('.titleClass span'); // 跟上面不同地方在於，可以取得多筆資料
 
-for (var i = 0; i < el.length; i++) {
-    el[i].textContent = '編號' + i;
-}
+var el = document.querySelector('.titleClass a')
+
+el.setAttribute('href','https://www.google.com/')
