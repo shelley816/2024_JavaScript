fa7e558f67409cd388c570ecf85b536d922c95d6
Shelley <jialing80916@gmail.com>
2024-06-25 18:46:10 +0800

    use for() to repeat the infos put in html

--- class_01/js/all.js
+++ class_01/js/all.js
@@ -1,5 +1,7 @@
 // var el = document.getElementById('titleID'); // 讀取獨一無二的 ID，每個標籤只能有一個 ID
+// var el = document.querySelector('.titleClass span'); // 可抓標籤名稱, ID, Class... 只取第一筆資料
+var el = document.querySelectorAll('.titleClass span'); // 跟上面不同地方在於，可以取得多筆資料
 
-var el = document.querySelector('.titleClass span'); // 可抓標籤名稱, ID, Class...
-
-el.textContent = ' everyone';
+for (var i = 0; i < el.length; i++) {
+    el[i].textContent = '編號' + i;
+}
