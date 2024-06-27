92a586d73b81b09419b21543bd93071e38659821
Shelley <jialing80916@gmail.com>
2024-06-27 19:00:33 +0800

    learned event

--- class_01/js/all.js
+++ class_01/js/all.js
@@ -1,34 +1,11 @@
 
-// createElement
-var str = document.createElement('span');
-str.innerHTML = '1234';
+var btn = document.querySelector('.btn');
+btn.onclick = function(e){
+    console.log(e);
+};
 
-document.querySelector('#main2').appendChild(str);
-
-// createElement + for
-var store = [
-    {
-        manager: 'John',
-        year: 8,
-        staffName: ['Mary', 'Bibi', 'Fifi'],
-        productItem: 76
-    },{
-        manager: 'Lily',
-        year: 5,
-        staffName: ['Bobo', 'Karen', 'Shelly'],
-        productItem: 89
-    },{
-        manager: 'Katie',
-        year: 2,
-        staffName: ['Carol', 'Sam', 'Joyce'],
-        productItem: 66
-    }
-];
-
-var el2 = document.querySelector('.list');
-var storeLen = store.length;
-for (let i = 0; i < storeLen; i++) {
-    var str = document.createElement('li');
-    str.textContent = store[i].manager;
-    el2.appendChild(str);
-}
+// 比較新的寫法
+var btn2 = document.querySelector('.btn2');
+btn2.addEventListener('click',function(){
+    alert('hello2');
+}, false);
