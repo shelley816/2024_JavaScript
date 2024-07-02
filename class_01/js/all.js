879c5391ce319799942b10fc595dcd2f112f4f66
Shelley <jialing80916@gmail.com>
2024-07-02 16:35:52 +0800

    to learn what html i click

--- class_01/js/all.js
+++ class_01/js/all.js
@@ -1,10 +1,4 @@
-var elBox = document.querySelector('.box');
-elBox.addEventListener('click',function(e){
-    alert('我是Box');
-    e.stopPropagation();
-},false)
-
-var elBody = document.querySelector('.body');
-elBody.addEventListener('click',function(){
-    alert('我是Body');
-},false)
+var el = document.querySelector('.header');
+el.addEventListener('click',function(e){
+    console.log(e.target.nodeName);
+});
