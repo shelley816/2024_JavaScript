6cd39c862b28707507b15c170f31661b8e6e1b9f
Shelley <jialing80916@gmail.com>
2024-07-02 17:12:45 +0800

    to learn to use change and for() to get data

--- class_01/js/all.js
+++ class_01/js/all.js
@@ -1,4 +1,29 @@
-var el = document.querySelector('.header');
-el.addEventListener('click',function(e){
-    console.log(e.target.nodeName);
-});
+var area = document.querySelector('#areaId');
+var list = document.querySelector('.list');
+var country = [
+    {
+        farmer: '查理',
+        place: '前鎮區'
+    },
+    {
+        farmer: '卡斯伯',
+        place: '苓雅區'
+    }
+    ,{
+        farmer: '小花',
+        place: '苓雅區'
+    }
+];
+
+var len = country.length;
+function updateList(e){
+    var selectItem = e.target.value;
+    var str = '';
+    for (var i = 0; i < len; i++) {
+        if (selectItem == country[i].place) {
+            str += '<li>' + country[i].farmer + '</li>';
+        }
+    }
+    list.innerHTML = str;
+};
+area.addEventListener('change',updateList,false);
