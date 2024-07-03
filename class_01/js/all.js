ec701d380e57b8c0ca06f2a6d325a3922cf41cec
Shelley <jialing80916@gmail.com>
2024-07-03 20:18:47 +0800

    to learn to use blur

--- class_01/js/all.js
+++ class_01/js/all.js
@@ -1,18 +1,11 @@
-var body = document.body;
 
 
-function goRocket(e){
-    switch(e.keyCode){
-        case 90:
-            document.querySelector('.rocket-1').style.bottom = '2000px';
-            break;
-        case 88:
-            document.querySelector('.rocket-2').style.bottom = '2000px';
-            break;
-        case 67:
-            document.querySelector('.rocket-3').style.bottom = '2000px';
-            break;
+function checkContent(e){
+    var str = e.target.value;
+    if(str==''){
+        alert('請輸入數量');
     }
 }
 
-body.addEventListener('keydown',goRocket,false);
+var inputHam = document.querySelector('#hamNumId');
+inputHam.addEventListener('blur',checkContent,false)
