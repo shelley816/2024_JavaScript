0bc3e03858e3309fc7ad52c560015fca485e1899
Shelley <jialing80916@gmail.com>
2024-07-09 19:35:23 +0800

    learn to turn array into string

--- class_01/js/all.js
+++ class_01/js/all.js
@@ -1,12 +1,7 @@
-var btn = document.querySelector('.btnClass');
-var call = document.querySelector('.btnCall');
-
-function saveName(e){
-    var str = document.querySelector('.textClass').value;
-    localStorage.setItem('myName',str);
-}
-btn.addEventListener('click',saveName);
-call.addEventListener('click',function(){
-    var str = localStorage.getItem('myName');
-    alert('我是'+str);
-},false)
+var stories = [
+    {manager:'Mary'},{manager:'Carol'}
+];
+var storiesStr = JSON.stringify(stories); //將 array 轉譯為 string
+localStorage.setItem('managers',storiesStr); //轉成 string 才能放入 localStorage
+var storiesAr = JSON.parse(storiesStr); //再將 string 轉譯為 array
+console.log(storiesAr[1].manager); //轉成 array 才能取出資料
