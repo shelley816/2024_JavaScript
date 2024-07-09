6a9ded7adcbb12dc34a66e6f0dda0c6b9197a67d
Shelley <jialing80916@gmail.com>
2024-07-09 14:11:24 +0800

    to learn localStorage's setItem, getItem

--- class_01/js/all.js
+++ class_01/js/all.js
@@ -1,22 +1,12 @@
-var el = document.body;
+var btn = document.querySelector('.btnClass');
+var call = document.querySelector('.btnCall');
 
-var screenX = document.querySelector('.screenX');
-var screenY = document.querySelector('.screenY');
-var pageX = document.querySelector('.pageX');
-var pageY = document.querySelector('.pageY');
-var clientX = document.querySelector('.clientX');
-var clientY = document.querySelector('.clientY');
-var mouseImg = document.querySelector('.mouseImg');
-
-el.addEventListener('mousemove',getMouseValue,false)
-
-function getMouseValue(e){
-    screenX.textContent = e.screenX;
-    screenY.textContent = e.screenY;
-    pageX.textContent = e.pageX;
-    pageY.textContent = e.pageY;
-    clientX.textContent = e.clientX;
-    clientY.textContent = e.clientY;
-    mouseImg.style.left = e.clientX + 'px';
-    mouseImg.style.top = e.clientY + 'px';
+function saveName(e){
+    var str = document.querySelector('.textClass').value;
+    localStorage.setItem('myName',str);
 }
+btn.addEventListener('click',saveName);
+call.addEventListener('click',function(){
+    var str = localStorage.getItem('myName');
+    alert('我是'+str);
+},false)
