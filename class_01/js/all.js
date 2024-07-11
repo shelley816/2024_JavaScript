affbaa33791fc6889ac0b49f8758e10899c9f8cf
Shelley <jialing80916@gmail.com>
2024-07-11 18:01:00 +0800

    to learn to use dataset + if + array

--- class_01/js/all.js
+++ class_01/js/all.js
@@ -1,8 +1,38 @@
-var list = document.querySelector('.list li');
-function checkData(e){
-    var num = e.target.dataset.num;
-    var cat = e.target.dataset.cat;
-    console.log('編號是：'+num);
-    console.log('養'+cat+'隻貓');
+var store = [
+    {
+        manager: 'John',
+        year: 8,
+        staffName: ['Mary', 'Bibi', 'Fifi'],
+        productItem: 76
+    },{
+        manager: 'Lily',
+        year: 5,
+        staffName: ['Bobo', 'Karen', 'Shelly'],
+        productItem: 89
+    },{
+        manager: 'Katie',
+        year: 2,
+        staffName: ['Carol', 'Sam', 'Joyce'],
+        productItem: 66
+    }
+];
+
+var list = document.querySelector('.list');
+function updateList(){
+    var storeLen = store.length;
+    var str = '';
+    for (var i = 0; i < storeLen; i++) {
+        str += "<li data-num='"+i+"'>"+store[i].manager+"</li>";
+        list.innerHTML = str;
+    }
 }
-list.addEventListener('click',checkData,false);
+updateList();
+
+function checkList(e){
+    var type = e.target.nodeName;
+    if(type !== 'LI'){return};
+    var num = e.target.dataset.num;
+    console.log(store[num].productItem);
+}
+
+list.addEventListener('click',checkList);
