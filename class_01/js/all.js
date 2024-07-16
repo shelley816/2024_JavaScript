fc88087c9e9c70abe36cc7bf41d709191284ea70
Shelley <jialing80916@gmail.com>
2024-07-16 18:24:26 +0800

    add list push into localstorage and  update list...

--- class_01/js/all.js
+++ class_01/js/all.js
@@ -1,24 +1,39 @@
-var inputCon = [];
-var clickBtn = document.querySelector('.clickBtn');
+// 指定 dom
+var list = document.querySelector('.list');
+var sendData = document.querySelector('.send')
+var data = JSON.parse(localStorage.getItem('listData')) || [];
 
-function getInputData(){
-    //取得輸入框文字
-    var str = document.querySelector('.inputItem').value;
-    updateList();
-    
-    //塞入陣列
-    inputCon.push(str);
-    //將陣列轉成字串
-    var inputConStr = JSON.stringify(inputCon);
-    //將轉成字串的陣列放到localStorage
-    localStorage.setItem('listItem',inputConStr);
-    //將localStorage的字串轉成陣列
-    var inputConaAr = JSON.parse(localStorage.listItem);
-    
+// 監聽與更新
+sendData.addEventListener('click', addData);
+list.addEventListener('click', toggleDone);
+updateList(data);
 
-    console.log(inputConaAr);
+// 加入列表，並同步更新網頁與 localstorage
+function addData(e){
+    e.preventDefault();
+    var txt = document.querySelector('.inputItem').value;
+    var todo = {
+        content: txt
+    };
+    data.push(todo);
+    updateList(data);
+    localStorage.setItem('listData', JSON.stringify(data));
 }
 
+// 更新網頁內容
+function updateList(items){
+    str = '';
+    var len = items.length;
+    for (var i = 0; i < len; i++) {
+        str += '<li><a href="#" data-num="' + i + '"+>刪除</a> <span>' + items[i].content + '</span></li>'
+    }
+    list.innerHTML = str;
+}
 
-
-clickBtn.addEventListener('click',getInputData,false);
+function toggleDone(e){
+    e.preventDefault();
+    if(e.target.tagName !== 'A'){return};
+    var num = e.target.dataset.num;
+    data.splice(num, 1);
+    updateList(data);
+}
