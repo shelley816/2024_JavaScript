37103d45f39013e8ae754be6397743e56c8ca436
Shelley <jialing80916@gmail.com>
2024-08-12 18:41:38 +0800

    AJAX demo

--- class_01/js/all.js
+++ class_01/js/all.js
@@ -1,10 +1,26 @@
-var account = {
-    email: 'sskkeel@gmail.com',
-    password: '4568912'
+var send = document.querySelector('.send');
+
+send.addEventListener('click',signup,false);
+
+function signup(){
+    var emailStr = document.querySelector('.account').value;
+    var passwordStr = document.querySelector('.password').value;
+    var account = {};
+    account.email = emailStr;
+    account.password = passwordStr;
+    
+    var xhr = new XMLHttpRequest();
+    xhr.open('post','https://hexschool-tutorial.herokuapp.com/api/signup',true);
+    xhr.setRequestHeader('Content-type','application/json');
+    var data = JSON.stringify(account);
+    xhr.send(data);
+    xhr.onload = function(){
+        var callbackData = JSON.parse(xhr.responseText);
+        var varStr = callbackData.message;
+        if(veriStr == '帳號註冊成功'){
+            alert('帳號註冊成功');
+        }else{
+            alert('帳號註冊失敗');
+        }
+    }
 }
-var xhr = new XMLHttpRequest();
-xhr.open('post','https://hexschool-tutorial.herokuapp.com/api/signup',true);
-xhr.setRequestHeader("Content-type","application/json");
-// 不能直接傳送物件，要把物件轉成字串
-var data = JSON.stringify(account);
-xhr.send(data);
