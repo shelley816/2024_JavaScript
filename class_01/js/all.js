d562459099da1f84ee6377746d006109d590a6fe
Shelley <jialing80916@gmail.com>
2024-08-13 16:42:09 +0800

    try to get openweather API

--- class_01/js/all.js
+++ class_01/js/all.js
@@ -1,26 +1,3 @@
-const send = document.querySelector('.send');
-
-send.addEventListener('click',login,false);
-
-function login(){
-    const emailStr = document.querySelector('.account').value;
-    const passwordStr = document.querySelector('.password').value;
-    let account = {};
-    account.email = emailStr;
-    account.password = passwordStr;
-
-    const xhr = new XMLHttpRequest();
-    xhr.open('post','https://escape-room.hexschool.io/api/user/signin',true);
-    xhr.setRequestHeader('Content-type','application/json');
-    let data = JSON.stringify(account);
-    xhr.send(data);
-    xhr.onload = function(){
-        let callbackData = JSON.parse(xhr.responseText);
-        let infoStr = callbackData.success;
-        if (infoStr == true){
-            alert(callbackData.message);
-        }else{
-            alert(callbackData.message);
-        }
-    }
-}
+const xhr = new XMLHttpRequest();
+xhr.open('get','http://api.openweathermap.org/data/2.5/forecast?id=7280291&appid=441d1e6a6de22a5b43d0cb54f643611b&lang=zh_tw&units=metric',true);
+xhr.send();
